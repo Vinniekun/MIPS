959f5699b6d035cd5f660ad85cf9b09197f5af40
cassiandrei <cschneider@inf.ufsm.br>
2015-05-28 18:23:59 -0300

    cosa

--- mips1.asm
+++ mips1.asm
@@ -1,5 +1,13 @@
 .text
+	main:
+		la $s0, Matriz
+		li $t0, 4
+		addi $t1, $s0, 36
+		loop:
+			sw $zero, 0($s0) 
+			add $s0, $s0, $t0
+			bne $s0, $t1, loop
+			 
 .data
-
 final: .word 1
 Matriz: .space 36
